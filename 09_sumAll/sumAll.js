9ed1fe53b5e7f8c4bb4057d7262f2fe619f28cbf
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 17:39:24 -0500

    Finish exercise number 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        let minNum = Math.min(a, b);
+        let maxNum = Math.max(a, b);
+        let result = 0;
+        for(let i = minNum; i <= maxNum; i++) {
+            result = result + i;
+            console.log("i = " + i);
+            console.log("result = " + result);
+        }
+        return result;
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
