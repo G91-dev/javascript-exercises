2e9fd9cc7510b8dae2ba2f5f30fd0b7f19a3bc2a
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 17:47:26 -0500

    Finish exercise number 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if(Number.isInteger(year/4)) {result = true};
+    if(Number.isInteger(year/100)) {result = false};
+    if(Number.isInteger(year/400)) {result = true};
+    return result;
 };
 
 // Do not edit below this line
