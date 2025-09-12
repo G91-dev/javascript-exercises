56bb81e85cd4eeb22c44acca22b780f6b0dc93af
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 15:31:19 -0500

    Finis exercise number 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    console.log("string: " + string);
+let reversed = "";
 
+for (let i = string.length - 1; i >= 0; i--) {
+    console.log("i: " + i);
+    reversed = reversed + string[i];
+    console.log("reversed: " + reversed);
+}
+
+return reversed;
 };
 
 // Do not edit below this line
