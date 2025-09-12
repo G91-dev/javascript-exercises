4423801d5d56357dd60e9f7c1c5fd79e269242d1
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 17:17:55 -0500

    Finish js exercise number 8
    While my code did comply with the requested conditions, it was much more complex than what it needed to be, according to the solution code. Will review in the future.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    if(arg.length <= 0) {
+        return "Error"
+    }
+
+    else {
+        for (let i = 0; i < arg.length ; i++) {
+            console.log("arr = " + arr);
+            console.log("taking out " + arg[i]);
+            let index = arr.indexOf(arg[i]);
+            if (index < 0) {continue}
+            else {
+                for (index ; index >= 0;) {
+                    console.log("index = " + index);
+                    arr.splice(index,1);
+                    console.log("arr after taking out " + arg[i] + " = "+ arr);
+                    index = arr.indexOf(arg[i]);
+                }
+            }
+        }
+        return arr
+    }
 };
+
+removeFromArray([1, 2, 3, 4, 5, 6, 7, 3], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
