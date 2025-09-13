29cfdbde8e5bdf54c59d470a424941abf1c237f0
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 21:30:18 -0500

    Finish javascript exercise number 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    console.log(arr);
+    let titlesArray = [];
+    for (let i = 0 ; i < arr.length; i++) {
+    titlesArray.push(arr[i].title);
+    console.log(titlesArray);
+    }
+    return titlesArray;
 };
 
 // Do not edit below this line
