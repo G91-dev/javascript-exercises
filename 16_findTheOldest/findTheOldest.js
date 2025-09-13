96639bc5d3330e0654b6956c57d577b7f19d6aba
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 23:11:40 -0500

    Finish javascript exercise number 16 - final test
    I learned a lot during this exercises, I'm sure I'll have to review them in the future. Some of the exercises had easier solutions using methods that I don't fully know at the moment.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.map (
+        (person) => {
+            if(typeof(person.yearOfDeath) == 'undefined') {
+            person.yearOfDeath = new Date().getFullYear()}
+        });
+    let ages = [];
+    console.log(ages);
+    let maxAge;
+    console.log(maxAge);
 
+    people.map ((person) => {ages.push(person.yearOfDeath - person.yearOfBirth)});
+    console.log(ages);
+    maxAge = Math.max(...ages);
+    console.log(maxAge);
+    return people.find (person => (person.yearOfDeath - person.yearOfBirth) === maxAge);
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
