8787e448cef02e26ac4ef2421544df7f6cfbcd03
Gustavo Cardenas <167231765+G91-dev@users.noreply.github.com>
2025-09-12 20:08:11 -0500

    Finish javascript exercise number 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let normalWord = word.toUpperCase().replace(/[^1-9A-Z]/g, '');
+    let reversed = "";
+    for( let i = normalWord.length - 1; i >= 0 ; i--) {
+        reversed = reversed + normalWord[i];
+        console.log (`i = ${i} // reversed = ${reversed}`)
+    }
+    console.log (`normalWord = ${normalWord}`)
+    console.log (`reversed = ${reversed}`)
+    return reversed == normalWord;
 };
 
 // Do not edit below this line
